6323c2de04320ed157ea693440eccc71ad455ee9
HemanthGK-GK <hemanthgk804@gmail.com>
2021-09-23 17:39:45 +0530

    Hemanth : Check for the option Ladder or Snake or NoPlay

---
+++ UC3SnakeLadderOptions/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace UC3SnakeLadderOptions
+{
+    public class SnakeLadder
+    {
+        static void Main(string[] args)
+        {
+            int position = 0;
+            int Max = 100;
+            int Player1 = 0;
+
+            int dice = neww.RollDice();
+            Console.WriteLine("Dice value is : " + dice);
+            int option = neww.getOptions();
+            Console.WriteLine("Option is : " + option);
+
+            switch (option)
+            {
+                case 2:
+                    position = position + dice;
+                    break;
+                case 1:
+                    if (position == 0)
+                        position = 0;
+                    else
+                    position = position - dice;
+                    break;
+                default:
+                    position = position;
+                    break;
+            }
+            Player1 = position;
+            Console.WriteLine("Player Position is : " + Player1);
+
+
+
+
+
+        }
+    }
+}
