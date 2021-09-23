a863e9899fe24504830df882d59b07fce827f1a9
HemanthGK-GK <hemanthgk804@gmail.com>
2021-09-23 17:37:40 +0530

    Hemanth : Get Dice number in SnakeLadder

---
+++ UC2SnakeLadder/Neww.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UC2SnakeLadder
+{
+    public class neww
+    {
+
+        public static int RollDice()
+        {
+            Random random = new Random();
+            int diceValue = random.Next(1, 7);
+            return diceValue;
+        }
+
+    }
+}
---
+++ UC2SnakeLadder/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace UC2SnakeLadder
+{
+    public class SnakeLadder
+    {
+        static void Main(string[] args)
+        {
+            int position = 0;
+            int Max = 100;
+            int Player1 = 0;
+
+            int dice = neww.RollDice();
+            Console.WriteLine("Dice value is : " + dice);
+        }
+    }
+}
