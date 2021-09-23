030b6c5b80baf30134fa99a50607fbcbda88a200
HemanthGK-GK <hemanthgk804@gmail.com>
2021-09-23 17:34:22 +0530

    Hemanth : Snake and Ladder Program

---
+++ UC1SnakeLadder/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace UC1SnakeLadder
+{
+    public class SnakeLadder
+    {
+        static void Main(string[] args)
+        {
+            int position = 0;
+            int Max = 100;
+            int Player1 = 0;
+
+            int dice = neww.RollDice();
+            Console.WriteLine("Dice value is : " + dice);
+            int option= neww.getOptions();
+
+            if (option == 2)
+                position = position + dice;
+            else if (option == 1)
+                position = position - dice;
+            else
+                position = position;
+
+
+
+
+        }
+    }
+}
